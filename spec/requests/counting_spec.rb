41a88ad0916db8593264c6295933fd76d046a71b
Dan Evron <dan.evron@klarna.com>
2018-03-28 23:13:02 +0300

    Adds API tests for happy case

--- spec/requests/counting_spec.rb
+++ spec/requests/counting_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Counting API', type: :request do
   let(:endpoint) { '/count' }
 
+  before do
+    @redis = Redis.new
+    @redis.flushall
+  end
+
   describe "POST count" do
     context "when posted data is valid" do
       let(:valid_data) { { text: "sometext" } }
@@ -63,6 +68,33 @@ RSpec.describe 'Counting API', type: :request do
         it "returns status code 422" do
           expect(response.status).to eq(422)
         end
+      end
+    end
+
+    context "when queried word does not appear" do
+      before { get endpoint, { params: { word: "does_not_exist" } } }
+
+      it "returns 0" do
+        expect(response.body).to eq("0")
+      end
+
+      it "returns status code 200" do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "when queried word appears" do
+      before do
+        @redis.set("wordcount:5_times", 5)
+        get endpoint, { params: { word: "5_times" } }
+      end
+
+      it "returns the word count" do
+        expect(response.body).to eq("5")
+      end
+
+      it "returns status code 200" do
+        expect(response.status).to eq(200)
       end
     end
   end
